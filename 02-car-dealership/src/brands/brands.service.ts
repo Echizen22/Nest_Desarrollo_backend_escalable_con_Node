a6315abaa4b7d75a3b7fcae28186206e411ad63b
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-10-14 01:07:14 +0200

    Crear CRUD completo de Brands

--- 02-car-dealership/src/brands/brands.service.ts
+++ 02-car-dealership/src/brands/brands.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from "uuid";
+
+import { Brand } from './entities/brand.entity';
+
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
 
 @Injectable()
 export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime()
+    }
+  ];
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+
+    const { name } = createBrandDto;
+
+    const brand: Brand = {
+      id: uuid(),
+      name: name.toLocaleLowerCase(),
+      createdAt: new Date().getTime(),
+    }
+
+    this.brands.push( brand );
+
+    return brand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.brands.find( brand => brand.id === id );
+    if ( !brand ) 
+      throw new NotFoundException(`Brand with id "${ id }" not found`);
+
+    return brand;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    
+    let brandDb = this.findOne( id );
+
+    this.brands = this.brands.map( brand => {
+      if( brand.id === id ) {
+        brandDb.updatedAt = new Date().getTime();
+        brandDb = { ...brandDb, ...updateBrandDto}
+        return brandDb;
+      }
+      return brand;
+    });
+
+    return brandDb;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    this.brands = this.brands.filter( brand => brand.id !== id );
   }
 }
