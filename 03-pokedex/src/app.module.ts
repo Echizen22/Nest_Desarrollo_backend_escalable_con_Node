2f33b542f7a576eaa8093cabaf8843c15a1f74f4
Erixon Arce Justiniano <earce@odec.es>
2024-10-14 16:59:38 +0200

    POST - Recibir y validar la data

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -13,7 +13,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
       rootPath: join(__dirname,'..','public'),
       }),
 
-      MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
     PokemonModule,
   ],
