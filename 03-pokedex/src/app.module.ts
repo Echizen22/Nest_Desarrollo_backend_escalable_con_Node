791aee35460b0c04e8ff9dfaa47aa959ac7d1179
Erixon Arce Justiniano <earce@odec.es>
2024-10-14 16:19:47 +0200

    Conectar Nest con Mongo

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,14 +1,21 @@
 import { join } from 'path'; // en Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
+
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
-    PokemonModule
+
+      MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
+    PokemonModule,
   ],
 })
 export class AppModule {}
