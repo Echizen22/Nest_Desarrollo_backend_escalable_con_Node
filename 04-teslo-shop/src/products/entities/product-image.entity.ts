92d9df40ea5989e29b1e8ae2e4d95b7741a83d6a
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-10-19 20:55:32 +0200

    OneToMany y ManyToOne

--- 04-teslo-shop/src/products/entities/product-image.entity.ts
+++ 04-teslo-shop/src/products/entities/product-image.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "./";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class ProductImage {
@@ -8,5 +9,11 @@ export class ProductImage {
 
     @Column('text')
     url: string;
+
+    @ManyToOne(
+        () => Product,
+        ( product ) => product.images
+    )
+    product: Product;
 
 }
