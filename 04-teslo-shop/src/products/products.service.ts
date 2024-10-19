4391ad51dab7af66fd873552d08112c1d62d4cbc
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-10-19 23:30:39 +0200

    Query Runner

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -21,6 +21,8 @@ export class ProductsService {
 
     @InjectRepository(ProductImage)
     private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSource: DataSource,
 
   ) {}
 
@@ -97,14 +99,19 @@ export class ProductsService {
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
+
+
+    const { images, ...toUpdate } = updateProductDto;
+
     
-    const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: [],
-    });
+    const product = await this.productRepository.preload({ id, ...toUpdate });
 
     if( !product ) throw new NotFoundException(`Product with id: ${ id } not found`);
+
+    // Create query runner
+    const queryRunner = this.dataSource.createQueryRunner();
+
+
     
     try {
       await this.productRepository.save( product );
