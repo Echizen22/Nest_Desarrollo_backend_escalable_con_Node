a60a6c78747e343e55692093f199f6f0b33257fb
Erixon Arce Justiniano <earce@odec.es>
2024-10-21 16:34:31 +0200

    Módulos Asíncronos

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
@@ -15,6 +16,20 @@ import { User } from './entities/user.entity';
 
     PassportModule.register({ defaultStrategy: 'jwt'}),
 
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        // console.log('JWT Secret', configService.get('JWT_SECRET'));
+        // console.log('JWT SECRET', process.env.JWT_SECRET);
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn:'2h'
+          }
+        }
+      }
+    })
     // JwtModule.register({
     //   secret: process.env.JWT_SECRET,
     //   signOptions: {
