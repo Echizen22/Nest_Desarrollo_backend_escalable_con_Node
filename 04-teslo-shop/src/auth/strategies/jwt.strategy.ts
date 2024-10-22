769dc0d68a5174f0932bf05f3195641f287c06e8
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-10-22 10:57:00 +0200

    Private Route - General

--- 04-teslo-shop/src/auth/strategies/jwt.strategy.ts
+++ 04-teslo-shop/src/auth/strategies/jwt.strategy.ts
@@ -33,7 +33,7 @@ export class JwtStrategy extends PassportStrategy( Strategy ) {
         if( !user )
             throw new UnauthorizedException('Token not valid');
 
-        if( user.isActive )
+        if( !user.isActive )
             throw new UnauthorizedException('User is inactive, talk with an admin');
 
 
